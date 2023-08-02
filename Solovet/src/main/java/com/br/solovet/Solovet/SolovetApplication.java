5383675d8567133d8e7568d2bfe11698a88be809
Luis Gustavo Pereira <gustavosbpereira@outlook.com>
2023-08-02 11:51:38 -0300

    first commit of project.

---
+++ Solovet/src/main/java/com/br/solovet/Solovet/SolovetApplication.java
@@ -0,0 +1,13 @@
+package com.br.solovet.Solovet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolovetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolovetApplication.class, args);
+	}
+
+}
---
+++ Solovet/src/test/java/com/br/solovet/Solovet/SolovetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.solovet.Solovet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolovetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
