4cbb41c3ad875aa3f5d3b4fc1c06c85139f678e5
Luis Gustavo Pereira <gustavosbpereira@outlook.com>
2023-08-24 20:43:22 -0300

    Fixed a bug in json.

---
+++ Solovet/src/main/java/com/br/solovet/Solovet/Infra/Secutiry/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.br.solovet.Solovet.Infra.Secutiry;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Mapeia todos os endpoints da API
+                .allowedOrigins("*") // Domínio permitido (troque pelo domínio correto)
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                .allowedHeaders("*"); // Headers permitidos
+    }
+}
