8fa3db7a1bf7f0b774f96a4bfae73d9d832f0b7d
Luis Gustavo Pereira <gustavosbpereira@outlook.com>
2023-08-26 06:02:09 -0300

    Syncying up frontend

--- siteSolovet/login.js
+++ siteSolovet/login.js
@@ -1,20 +1,34 @@
-document.getElementById("submit").addEventListener("click", login( document.getElementById("login").innerHTML, document.getElementById("password").innerHTML)); 
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar o comportamento padrão do formulário
+
+    const username = document.getElementById("login").value;
+    const password = document.getElementById("password").value;
+
+    login(username, password);
+}); 
 
 async function login(username, password){
     console.log("teste");
     try{
         console.log("teste");
-        const response = await fetch("http://localhost:8080/usuarios/login",{
+        const response = await fetch("http://localhost:8080/usuarios/login", {
             
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: {
-                "username":username,
-                "password":password
-            }
-        })
+            body: JSON.stringify({
+                username:username,
+                password:password
+            })
+
+        });
+        if(response.ok){
+            const data = await responde.json();
+            console.log("Login bem sucedido: ", data);
+        } else {
+            console.error("Erro no login:", response.statusText)
+        }
     } catch (error) {
         console.error("Error:", error);
       }
