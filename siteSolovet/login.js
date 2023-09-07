760f9fc757ae5bf4cd1a671c56d02d46a824e9e9
Gustavo <gustavosbpereira@outlook.com>
2023-09-07 17:48:12 -0300

    -m"Rolling back to watch the response"

--- siteSolovet/login.js
+++ siteSolovet/login.js
@@ -50,7 +50,7 @@ async function login(username, password){
 
         if(response.ok){
             const data = await response.json();
-            console.log("Login bem sucedido: ", data.stringify(Object.token));
+            console.log("Login bem sucedido: ", data);
             localStorage.setItem("tokenJWT", data);
             localStorage.setItem("email", username);
             location.href = "dashboard.html"
