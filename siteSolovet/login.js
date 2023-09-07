b7a487779dd819ba1936d6898d044c7d8b36a057
Gustavo <gustavosbpereira@outlook.com>
2023-09-07 16:52:56 -0300

    -m"testing"

--- siteSolovet/login.js
+++ siteSolovet/login.js
@@ -50,7 +50,7 @@ async function login(username, password){
 
         if(response.ok){
             const data = await response.json();
-            console.log("Login bem sucedido: ", data);
+            console.log("Login bem sucedido: ", data.token);
             localStorage.setItem("tokenJWT", response);
             localStorage.setItem("email", username);
             location.href = "dashboard.html"
